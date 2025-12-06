ae1800afa4d0509f9507c999963380fc46df9a5e
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-12-06 09:27:58 +0700

    Feat: Created database connector

--- src/data_access/DatabaseConnector.java
+++ src/data_access/DatabaseConnector.java
@@ -1,5 +1,23 @@
 package data_access;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnector {
+	
+	private static Connection conn;
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ooad_project", "root", "");
+			} catch (SQLException e) {
+				System.out.println("Failed in connecting to database.");
+				e.printStackTrace();
+			}
+		}
+		return conn;
+	}
 
 }
