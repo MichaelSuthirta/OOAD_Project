47f985aebedce8421f0a1def8c6ba986867e7ccd
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-12-07 11:40:30 +0700

    Feat: Prepared repository classes

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,31 +1,12 @@
 package controller;
 
+import data_access.UserRepository;
 import models.User;
 
 public class UserController {
 
-	//Customer is the one who can register
-	public static User registerUser(String name, String email, String password, String phone,
-			String address, String gender) {
-		
-		//Insert data into DB
-		return null;
-	}
-	
-	public static User getUserByEmail(String email) {
-		//find email in DB
-		return null;
-	}
-	
-	public static User login(String email, String password) {
-		User user = getUserByEmail(email);
-		
-		if(user == null) {
-			return null;
-		}
-		else {
-			//Validate password, create user, return user
-			return null;
-		}
+	public static User getUserByID(String ID) {
+		User result = UserRepository.findUserByID(Integer.parseInt(ID));
+		return result;
 	}
 }
--- src/models/User.java
+++ src/models/User.java
@@ -1,6 +1,8 @@
 package models;
 
-public abstract class User {
+import controller.UserController;
+
+public class User {
 	private String idUser, fullName, email, password, phone, address, gender, role;
 
 	public User(String idUser, String fullName, String email, String password, String phone, String address,
@@ -16,5 +18,64 @@ public abstract class User {
 		this.role = role;
 	}
 
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public User getUser(String id) {
+		return UserController.getUserByID(id);
+	}
 	
 }
