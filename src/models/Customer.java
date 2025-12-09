767126bef0fcad9b206eb5ef6918c5e2a3098026
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-12-09 09:55:04 +0700

    Feat: Added function to register user

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -5,7 +5,7 @@ public class Customer extends User{
 	
 	public Customer(String idUser, String fullName, String email, String password, String phone, String address,
 			String gender, double balance) {
-		super(idUser, fullName, email, password, phone, address, gender, "user");
+		super(idUser, fullName, email, password, phone, address, gender, "customer");
 		this.balance = 0;
 	}
 
