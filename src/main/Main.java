d5a791087ac6526de757c4cad25cbf8964dfcbd4
Gilmore <gilmoree937@gmail.com>
2025-12-15 16:02:24 +0700

    delete LoginController

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import controller.LoginController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,7 +7,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		new LoginController(primaryStage);
+		
 	}
 	
 	public static void main(String[] args) {
